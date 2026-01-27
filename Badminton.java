10c39cc7983765df9d13123a69327f7e078a1ccf
Gururaj B T <gubt@cisco.com>
2026-01-27 14:38:28 +0530

    test

--- Badminton.java
+++ Badminton.java
@@ -1,5 +1,5 @@
 public interface Badminton {
-    public void serve(String serveType);
+    public void serve(int serveType);
     public void smash(int speed);
     public void rally(int durationInSeconds);
 }
